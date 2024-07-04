97c52e4df076dbf2848b3a8e603efb23ec8f6815
asheneo <subodja162@gmail.com>
2024-07-04 14:31:40 +0530

    models for places and activities

--- cms/models.py
+++ cms/models.py
@@ -17,3 +17,33 @@ class EmailConfirmation(TimeStampMixin):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, related_name="email_confirmation", on_delete=models.CASCADE)
     code = models.CharField(max_length=100)
+
+class Place(TimeStampMixin):
+    place_id = models.AutoField(primary_key=True)
+    place_name = models.CharField(max_length=255)
+    description = models.TextField()
+    district = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    how_to_visit = models.TextField()
+    accommodation_places_nearby = models.TextField()
+    best_time_to_visit_in_year = models.CharField(max_length=100)
+    tags = models.CharField(max_length=255)
+    activities = models.ManyToManyField("Activity", related_name="places", through="PlaceActivity")
+    data = models.JSONField()
+
+class Activity(TimeStampMixin):
+    activity_id = models.AutoField(primary_key=True)
+    activity_name = models.CharField(max_length=255)
+    description = models.TextField()
+    data = models.JSONField()
+
+class PlaceActivity(TimeStampMixin):
+    id = models.AutoField(primary_key=True)
+    place = models.ForeignKey(Place, related_name="place_activity", on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, related_name="place_activity", on_delete=models.CASCADE)
+    description = models.TextField()
+    data = models.JSONField()
