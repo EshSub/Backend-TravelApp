9e5d06990444cbfaf71059ce7d24a10fa132067b
ashaneo <subodha162@gmail.com>
2024-08-30 20:43:42 +0530

    db changes

--- messaging/models.py
+++ messaging/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Conversation(models.Model):
     conversation_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    isAI = models.BooleanField()
+    isAI = models.BooleanField(default=True)
 
     def __str__(self):
         return f"Conversation {self.conversation_id} - User {self.user_id}"
@@ -15,9 +15,9 @@ class Message(models.Model):
     date = models.DateField()
     time = models.TimeField()
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-    message = models.TextField()
-    place = models.ForeignKey(Place, null=True, blank=True, on_delete=models.PROTECT)
-    guide = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT)
+    message = models.TextField() 
+    place = models.ForeignKey(Place, null=True, blank=True, on_delete=models.PROTECT)  
+    guide = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT) 
 
     def __str__(self):
         return f"Message in Conversation {self.conversation.conversation_id} on {self.date} at {self.time}"
