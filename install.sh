2272b38d2b3635d6ec6d1d6d4a4ec450d472b5b5
eshans <eshans2000@gmail.com>
2024-09-02 23:00:21 +0530

    update install sh

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
+# install python3-pip
+sudo apt-get install python3-pip -y
+
+# install venv
+sudo apt-get install python3-venv -y
+
+# create venv
+python3 -m venv venv
+
+# activate venv
+source venv/bin/activate
+
+# install requirements
+pip install -r requirements.txt
+
+# install gunicorn
+pip install gunicorn
+
 # add log files for gunicorn
 sudo mkdir -pv /var/{log,run}/gunicorn/
 
