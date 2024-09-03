55a8f985fef2f62955d3ecfbac451541659ff114
eshans <eshans2000@gmail.com>
2024-09-03 18:35:28 +0530

    add test email

--- cms/views.py
+++ cms/views.py
@@ -1,53 +1,103 @@
 from rest_framework import viewsets
-from .models import Profile, EmailConfirmation, District, Province, Place, Activity, PlaceActivity, Type, Tag
-from .serializers import ProfileSerializer, EmailConfirmationSerializer, DistrictSerializer, ProvinceSerializer, PlaceSerializer, ActivitySerializer, PlaceActivitySerializer, TypeSerializer, TagSerializer
+from .models import (
+    Profile,
+    EmailConfirmation,
+    District,
+    Province,
+    Place,
+    Activity,
+    PlaceActivity,
+    Type,
+    Tag,
+)
+from .serializers import (
+    ProfileSerializer,
+    EmailConfirmationSerializer,
+    DistrictSerializer,
+    ProvinceSerializer,
+    PlaceSerializer,
+    ActivitySerializer,
+    PlaceActivitySerializer,
+    TypeSerializer,
+    TagSerializer,
+)
 from math import radians, sin, cos, sqrt, atan2
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions
+from django.core.mail import send_mail
+
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
+
 class EmailConfirmationViewSet(viewsets.ModelViewSet):
     queryset = EmailConfirmation.objects.all()
     serializer_class = EmailConfirmationSerializer
+
+    @action(detail=False, methods=["get"])
+    def test_email(self, request):
+        res = (
+            send_mail(
+                "Test email",
+                "This is a test email",
+                "touracross@gmail.com",
+                ["eshans2000@gmail.com"]
+            ),
+        )
+        return Response({"message": res})
+
 
 class DistrictViewSet(viewsets.ModelViewSet):
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
 
+
 class ProvinceViewSet(viewsets.ModelViewSet):
     queryset = Province.objects.all()
     serializer_class = ProvinceSerializer
 
+
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
-    filterset_fields = ['district','activities']
+    filterset_fields = ["district", "activities"]
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    @action(detail=True, methods=['get'])
+    @action(detail=True, methods=["get"])
     def nearby_accommodations(self, request, pk=None):
         place = self.get_object()
         if not place.latitude or not place.longitude:
-            return Response({"error": "Place does not have valid latitude and longitude coordinates."}, status=400)
+            return Response(
+                {
+                    "error": "Place does not have valid latitude and longitude coordinates."
+                },
+                status=400,
+            )
 
         nearby_places = Place.objects.filter(types__name="Hotel")
         nearby_accommodations = []
         for nearby_place in nearby_places:
             if nearby_place.latitude and nearby_place.longitude:
-                distance = self.calculate_distance(place.latitude, place.longitude, nearby_place.latitude, nearby_place.longitude)
+                distance = self.calculate_distance(
+                    place.latitude,
+                    place.longitude,
+                    nearby_place.latitude,
+                    nearby_place.longitude,
+                )
                 if distance < 10:  # distance in kilometers
-                    nearby_accommodations.append({
-                        "place_id": nearby_place.place_id,
-                        "place_name": nearby_place.place_name,
-                        "description": nearby_place.description,
-                        "distance_km": round(distance, 2)
-                    })
+                    nearby_accommodations.append(
+                        {
+                            "place_id": nearby_place.place_id,
+                            "place_name": nearby_place.place_name,
+                            "description": nearby_place.description,
+                            "distance_km": round(distance, 2),
+                        }
+                    )
 
         return Response(nearby_accommodations)
 
@@ -56,30 +106,39 @@ class PlaceViewSet(viewsets.ModelViewSet):
         R = 6371  # Radius of the earth in km
         dlat = radians(lat2 - lat1)
         dlon = radians(lon2 - lon1)
-        a = sin(dlat / 2) * sin(dlat / 2) + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) * sin(dlon / 2)
+        a = sin(dlat / 2) * sin(dlat / 2) + cos(radians(lat1)) * cos(
+            radians(lat2)
+        ) * sin(dlon / 2) * sin(dlon / 2)
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
         distance = R * c  # Distance in km
         return distance
+
 
 class ActivityViewSet(viewsets.ModelViewSet):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
-    filterset_fields = ['places']
-    ordering_fields = ['activity_id', 'activity_name', 'place_activities', 'places']  # Specify the fields that can be used for ordering
-    ordering = ['activity_id'] # Specify the default ordering
+    filterset_fields = ["places"]
+    ordering_fields = [
+        "activity_id",
+        "activity_name",
+        "place_activities",
+        "places",
+    ]  # Specify the fields that can be used for ordering
+    ordering = ["activity_id"]  # Specify the default ordering
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 class PlaceActivityViewSet(viewsets.ModelViewSet):
     queryset = PlaceActivity.objects.all()
     serializer_class = PlaceActivitySerializer
 
+
 class TypeViewSet(viewsets.ModelViewSet):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
 
+
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-
-
