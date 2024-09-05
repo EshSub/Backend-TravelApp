9e5b34552d1e9aa926325afb3d1151dbeea57b39
inuka-00 <77762018+inuka-00@users.noreply.github.com>
2024-09-05 10:25:04 +0530

    Added User info to login response

--- app/urls.py
+++ app/urls.py
@@ -17,14 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import CustomTokenObtainPairView  # Ensure this import points to your new view
 
 router = routers.DefaultRouter()
-
 urlpatterns = [
     path("", include('cms.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
 ]
